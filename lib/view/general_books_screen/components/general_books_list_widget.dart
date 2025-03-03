adfe959367696b5a92b02433961df22daf4d1217
saeid721 <saeid721km@gmail.com>
2025-03-03 16:06:05 +0600

    update

--- lib/view/general_books_screen/components/general_books_list_widget.dart
+++ lib/view/general_books_screen/components/general_books_list_widget.dart
@@ -5,6 +5,8 @@ import '../../../global/constants/colors_resources.dart';
 class GeneralBookListWidget extends StatelessWidget {
   final VoidCallback onTap;
   final String bookName;
+  final String? authorName;
+  final String? shortDescription;
   final Color bookNameColor;
   final Color authorNameColor;
   final Color shortDescriptionColor;
@@ -16,6 +18,8 @@ class GeneralBookListWidget extends StatelessWidget {
   const GeneralBookListWidget({
     required this.onTap,
     required this.bookName,
+    this.authorName,
+    this.shortDescription,
     this.bookNameColor = ColorRes.primaryColor,
     this.authorNameColor = ColorRes.textColor,
     this.shortDescriptionColor = ColorRes.textColor,
@@ -30,59 +34,108 @@ class GeneralBookListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Card(
-        color: ColorRes.white,
-        elevation: 1,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: Get.width,
-              child: Material(
-                color: Colors.transparent,
-                shadowColor: Colors.black.withOpacity(1),
-                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                child: Container(
-                  width: Get.width,
-                  height: 205,
-                  decoration: BoxDecoration(
-                    color: ColorRes.white,
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(10.0),
-                      topRight: Radius.circular(10.0),
-                    ),
-                    image: imageUrl != null
-                        ? DecorationImage(
-                      image: NetworkImage(imageUrl!),
-                      fit: BoxFit.cover,
-                    )
-                        : null,
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      bookName,
-                      textAlign: TextAlign.center,
-                      maxLines: 2,
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w500,
-                        color: bookNameColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, 5),
             ),
           ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(16),
+            onTap: onTap,
+            splashColor: ColorRes.primaryColor.withOpacity(0.1),
+            highlightColor: ColorRes.primaryColor.withOpacity(0.05),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Book cover with gradient overlay
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  child: Stack(
+                    children: [
+                      Hero(
+                        tag: 'book-${bookName.hashCode}',
+                        child: Container(
+                          width: Get.width,
+                          height: 225,
+                          decoration: BoxDecoration(
+                            color: ColorRes.primaryColor.withOpacity(0.1),
+                            image: imageUrl != null
+                                ? DecorationImage(
+                              image: NetworkImage(imageUrl!),
+                              fit: BoxFit.cover,
+                            )
+                                : null,
+                          ),
+                          child: imageUrl == null
+                              ? Icon(
+                            Icons.book,
+                            size: 50,
+                            color: ColorRes.primaryColor.withOpacity(0.3),
+                          )
+                              : null,
+                        ),
+                      ),
+                      // Gradient overlay for better text visibility
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.transparent,
+                                Colors.black.withOpacity(0.7),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Book information
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(3),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          bookName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            color: bookNameColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/view/general_books_screen/general_books_list_screen.dart
+++ lib/view/general_books_screen/general_books_list_screen.dart
@@ -7,6 +7,8 @@ import '../../../global/constants/colors_resources.dart';
 import '../../../global/widget/custom_bottom_navbar.dart';
 import '../../../global/widget/global_container.dart';
 import '../../../global/widget/global_sizedbox.dart';
+import '../../data/general_books_list.dart';
+import '../../data/trending_books_list.dart';
 import '../../global/custom_app_bar.dart';
 import '../../global/widget/global_progress_hub.dart';
 import '../download/components/download_model.dart';
@@ -19,11 +21,32 @@ class GeneralBooksListScreen extends StatefulWidget {
   State<GeneralBooksListScreen> createState() => _GeneralBooksListScreenState();
 }
 
-class _GeneralBooksListScreenState extends State<GeneralBooksListScreen> {
+class _GeneralBooksListScreenState extends State<GeneralBooksListScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    )..forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<PdfController>(builder: (pdfController) {
       return Scaffold(
+        extendBodyBehindAppBar: true,
         appBar: const CustomAppBar(
           title: 'General Books List',
         ),
@@ -33,52 +56,113 @@ class _GeneralBooksListScreenState extends State<GeneralBooksListScreen> {
             height: size(context).height,
             width: size(context).width,
             color: ColorRes.backgroundColor,
-            child: SingleChildScrollView(
+            child: SafeArea(
               child: Padding(
                 padding: const EdgeInsets.only(top: 10),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    GlobalContainer(
-                      width: Get.width,
-                      color: ColorRes.backgroundColor,
-                      child: GridView.builder(
-                        itemCount: recommendedBooks
-                            .length, // Should be generalBooks.length
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 5,
-                          crossAxisSpacing: 5,
-                          mainAxisExtent: 263,
+                    Expanded(
+                      child: pdfController.isLoading
+                          ? const Center(child: CircularProgressIndicator())
+                          : generalBooks.isEmpty
+                          ? Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.menu_book_rounded,
+                              size: 70,
+                              color: ColorRes.primaryColor
+                                  .withOpacity(0.3),
+                            ),
+                            const SizedBox(height: 16),
+                            Text(
+                              "No recommendations available",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: ColorRes.textColor
+                                    .withOpacity(0.7),
+                              ),
+                            ),
+                          ],
                         ),
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                      )
+                          : GridView.builder(
+                        controller: _scrollController,
+                        itemCount: generalBooks.length,
+                        padding:
+                        const EdgeInsets.fromLTRB(8, 0, 8, 10),
+                        physics: const BouncingScrollPhysics(),
+                        gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 0,
+                          crossAxisSpacing: 0,
+                          mainAxisExtent: 280,
+                        ),
                         itemBuilder: (ctx, index) {
-                          final book = recommendedBooks[
-                              index]; // Should use generalBooks[index]
-                          return GeneralBookListWidget(
-                            onTap: () {
-                              pdfController.setPdfUrl(book.pdfUrl);
-                              Get.to(() => UrlPdfScreen(
+                          final book = generalBooks[index];
+
+                          // Staggered animation for grid items
+                          return AnimatedBuilder(
+                            animation: _animationController,
+                            builder: (context, child) {
+                              final delay = index * 0.1;
+                              final animation =
+                              Tween<double>(begin: 0, end: 1)
+                                  .animate(
+                                CurvedAnimation(
+                                  parent: _animationController,
+                                  curve: Interval(
+                                    0.4 + delay > 1.0
+                                        ? 1.0
+                                        : 0.4 + delay,
+                                    0.7 + delay > 1.0
+                                        ? 1.0
+                                        : 0.7 + delay,
+                                    curve: Curves.easeOut,
+                                  ),
+                                ),
+                              );
+
+                              return FadeTransition(
+                                opacity: animation,
+                                child: SlideTransition(
+                                  position: Tween<Offset>(
+                                    begin: const Offset(0, 0.3),
+                                    end: Offset.zero,
+                                  ).animate(animation),
+                                  child: child,
+                                ),
+                              );
+                            },
+                            child: GeneralBookListWidget(
+                              onTap: () {
+                                pdfController.setPdfUrl(book.pdfUrl);
+                                Get.to(
+                                      () => UrlPdfScreen(
                                     downloadBooks: DownloadBooks(
                                       imageUrl: book.imageUrl,
                                       pdfUrl: book.pdfUrl,
                                       bookName: book.bookName,
                                       authorName: book.authorName,
                                       shortDescription:
-                                          book.shortDescription ?? '',
+                                      book.shortDescription ?? '',
                                     ),
-                                  ));
-                            },
-                            imageUrl: book.imageUrl,
-                            bookName: book.bookName,
+                                  ),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(
+                                      milliseconds: 300),
+                                );
+                              },
+                              imageUrl: book.imageUrl,
+                              bookName: book.bookName,
+                            ),
                           );
                         },
                       ),
                     ),
-                    sizedBoxH(10),
                   ],
                 ),
               ),
